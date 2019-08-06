9295c72559006347b374c394cc9df0b0b74bc442
RoundYuanYuan <mengyuan_per@outlook.com>
2019-08-06 18:36:58 +0800

    first commit

---
+++ castest/src/main/java/com/zmy/MyCas.java
@@ -0,0 +1,30 @@
+package com.zmy;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyCas extends HttpServlet {
+    private String message;
+
+    @Override
+    public void init() throws ServletException {
+        message="Hello World";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out=resp.getWriter();
+        out.println("<h1>"+message+"</h1>");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destory");
+        super.destroy();
+    }
+}
